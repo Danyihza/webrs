32f497a75427c67a727f97eb77c4e9c70ed539c9
Danyihza <55189649+Danyihza@users.noreply.github.com>
2021-09-27 20:50:50 +0700

    push tracer

---
+++ application/views/print/tracer.php
@@ -0,0 +1,442 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>REVIU RENCANA SKP</title>
+</head>
+
+<style type="text/css">
+    body {
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        background-color: #FAFAFA;
+        font-family: Arial, sans-serif;
+    }
+
+    * {
+        box-sizing: border-box;
+        -moz-box-sizing: border-box;
+    }
+
+    .page {
+        width: 210mm;
+        min-height: 297mm;
+        margin: 3mm auto;
+        box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+        -webkit-print-color-adjust: exact !important;
+    }
+
+    .subpage {
+        padding-top: 2px;
+        padding-left: 30px;
+        padding-right: 20px;
+        border: 1px;
+        height: 257mm;
+    }
+
+    @page {
+        size: A4;
+        margin: 0;
+    }
+
+    @media print {
+
+        html,
+        body {
+            width: 210mm;
+            height: 297mm;
+        }
+
+        .page {
+            margin: 0;
+            border: initial;
+            border-radius: initial;
+            width: initial;
+            min-height: initial;
+            box-shadow: initial;
+            background: initial;
+            page-break-after: always;
+        }
+    }
+</style>
+
+<style>
+    h4 {
+        text-align: center;
+    }
+
+    .sub {
+        font-size: 12px;
+        margin-top: 20px;
+    }
+
+    .tB {
+        border-collapse: collapse;
+        border-spacing: 0;
+    }
+
+    .tB td {
+        border-color: black;
+        border-style: solid;
+        border-width: 1px;
+        overflow: hidden;
+        padding: 2px 2px;
+        word-break: normal;
+    }
+
+    .tB th {
+        border-color: black;
+        border-style: solid;
+        border-width: 1px;
+        font-weight: normal;
+        overflow: hidden;
+        padding: 2px 2px;
+        word-break: normal;
+    }
+
+    .tB .tB-p9rb {
+        font-size: 11px;
+        text-align: center;
+        vertical-align: top
+    }
+
+    .tB .tB-p9rc {
+        font-size: 11px;
+        text-align: left;
+        vertical-align: top
+    }
+
+    .tg {
+        border-collapse: collapse;
+        border-spacing: 0;
+    }
+
+    .tg td {
+        border-color: black;
+        border-style: solid;
+        border-width: 1px;
+        font-size: 11px;
+        overflow: hidden;
+        padding: 1px 1px;
+        word-break: normal;
+    }
+
+    .tg th {
+        border-color: black;
+        border-style: solid;
+        border-width: 1px;
+        font-size: 11px;
+        font-weight: normal;
+        overflow: hidden;
+        padding:1px 1px;
+        word-break: normal;
+    }
+
+    .tg .tg-sg5v {
+        border-color: inherit;
+        text-align: left;
+        vertical-align: top
+    }
+
+    .tg .tg-c3ow {
+        border-color: inherit;
+        text-align: center;
+        vertical-align: top
+    }
+
+    .tg .tg-0pky {
+        border-color: inherit;
+        text-align: left;
+        vertical-align: top
+    }
+
+
+    .tc {
+        margin-top: 3mm;
+        margin-left: 120mm;
+        border-collapse: collapse;
+        border-spacing: 0;
+    }
+
+    .tc td {
+        font-size: 12px;
+        overflow: hidden;
+        padding: 3px 5px;
+        word-break: normal;
+        text-align: center;
+    }
+
+    .tc th {
+        font-size: 12px;
+        font-weight: normal;
+        overflow: hidden;
+        padding: 3px 5px;
+        word-break: normal;
+        text-align: center;
+    }
+
+    .tc .tg-jytt {
+        text-align: center;
+        vertical-align: top
+    }
+
+    .square {
+        margin-top: 10px;
+        margin-bottom: 10px;
+        height: 30px;
+        width: 40px;
+        border-style: solid;
+        border-width: 2px;
+        border-color: black;
+    }
+</style>
+
+<body>
+    <div class="form">
+        <div class="page">
+            <div class="subpage">
+                <h4>REVIU RENCANA SKP PEJABAT FUNGSIONAL</h4>
+                <table class="sub">
+                    <colgroup>
+                        <col style="width: 373px">
+                        <col style="width: 133px">
+                        <col style="width: 240px">
+                    </colgroup>
+                    <thead>
+                        <tr>
+                            <td>BIRO ORGANISASI DAN KEPEGAWAIAN</td>
+                            <td>PERIODE PENILAIAN : </td>
+                            <td>1 JULI S.D. 31 DESEMBER 2021</td>
+                        </tr>
+                    </thead>
+                </table>
+
+                <table class="tB" style="table-layout: fixed;">
+                    <colgroup>
+                        <col style="width: 140px">
+                        <col style="width: 233px">
+                        <col style="width: 140px">
+                        <col style="width: 230px">
+                    </colgroup>
+                    <thead>
+                        <tr>
+                            <th class="tB-p9rb" colspan="2">PEGAWAI YANG DINILAI</th>
+                            <th class="tB-p9rb" colspan="2">PEJABAT PENILAI KINERJA </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="tB-p9rc">NAMA</td>
+                            <td class="tB-p9rc">AGUNG DANANG RAHADI, SE</td>
+                            <td class="tB-p9rc">NAMA</td>
+                            <td class="tB-p9rc">INDRIA FITRIANI, SH, MAP</td>
+                        </tr>
+                        <tr>
+                            <td class="tB-p9rc">NIP</td>
+                            <td class="tB-p9rc">198508022009121002</td>
+                            <td class="tB-p9rc">NIP</td>
+                            <td class="tB-p9rc">197211061999032002</td>
+                        </tr>
+                        <tr>
+                            <td class="tB-p9rc">PANGKAT/GOL. RUANG</td>
+                            <td class="tB-p9rc">PENATA/IIIc</td>
+                            <td class="tB-p9rc">PANGKA/GOL. RUANG</td>
+                            <td class="tB-p9rc">PEMBINA TK.I/ IVb</td>
+                        </tr>
+                        <tr>
+                            <td class="tB-p9rc">JABATAN</td>
+                            <td class="tB-p9rc">ANALIS KEPEGAWAIAN MUDA selaku SUBKOORDINATOR</td>
+                            <td class="tB-p9rc">JABATAN </td>
+                            <td class="tB-p9rc">ANALIS KEPEGAWAIAN MADYA selaku</td>
+                        </tr>
+                        <tr>
+                            <td class="tB-p9rc">UNIT KERJA</td>
+                            <td class="tB-p9rc">BIRO ORGANISASI DAN KEPEGAWAIAN</td>
+                            <td class="tB-p9rc">UNIT KERJA</td>
+                            <td class="tB-p9rc">BIRO ORGANISASI DAN KEPEGAWAIAN</td>
+                        </tr>
+                        <tr>
+                            <td class="tB-p9rc"></td>
+                            <td class="tB-p9rc">SEKRETARIAT JENDRAL</td>
+                            <td class="tB-p9rc"></td>
+                            <td class="tB-p9rc">SEKRETARIAT JENDRAL</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <table class="tg" style="table-layout: fixed;">
+                    <colgroup>
+                        <col style="width: 30px">
+                        <col style="width: 90px">
+                        <col style="width: 80px">
+                        <col style="width: 80px">
+                        <col style="width: 92px">
+                        <col style="width: 60px">
+                        <col style="width: 60px">
+                        <col style="width: 60px">
+                        <col style="width: 80px">
+                        <col style="width: 110px">
+                    </colgroup>
+                    <thead>
+                        <tr>
+                            <th class="tg-c3ow" rowspan="2">NO</th>
+                            <th class="tg-c3ow" rowspan="2">RENCANA KINERJA ATASAN LANGSUNG / UNIT KERJA / ORGANISASI
+                            </th>
+                            <th class="tg-c3ow" rowspan="2">RENCANA KINERJA</th>
+                            <th class="tg-c3ow" rowspan="2">ASPEK</th>
+                            <th class="tg-c3ow" rowspan="2">INDIKATOR KINERJA INDIVIDU</th>
+                            <th class="tg-c3ow" rowspan="2">TARGET</th>
+                            <th class="tg-c3ow" colspan="2">KATEGORI PENILAIAN</th>
+                            <th class="tg-c3ow" rowspan="2">SUMBER DATA UNTUK PENGUKURAN</th>
+                            <th class="tg-c3ow" rowspan="2">REVIU OLEH PENGELOLA KINERJA</th>
+                        </tr>
+                        <tr>
+                            <th class="tg-c3ow">CUKUP</th>
+                            <th class="tg-c3ow">SANGAT BAIK</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="tg-c3ow">(1)</td>
+                            <td class="tg-c3ow">(2)</td>
+                            <td class="tg-c3ow">(3)</td>
+                            <td class="tg-c3ow">(4)</td>
+                            <td class="tg-c3ow">(5)</td>
+                            <td class="tg-c3ow">(6)</td>
+                            <td class="tg-c3ow">(7)</td>
+                            <td class="tg-c3ow">(8)</td>
+                            <td class="tg-c3ow">(9)</td>
+                            <td class="tg-c3ow">(10)</td>
+                        </tr>
+                        <tr>
+                            <td class="tg-0pky" colspan="10">A. KINERJA UTAMA</td>
+                        </tr>
+                        <tr>
+                            <td class="tg-c3ow" rowspan="4">1</td>
+                            <td class="tg-0pky" rowspan="4">Penataan Tata Laksana Kementan</td>
+                            <td class="tg-0pky">Penataan Dokumen Proses Bisnis Lingkup Kementan</td>
+                            <td class="tg-0pky">IKI (Kuantitas)</td>
+                            <td class="tg-0pky">Dokumen proses bisnis Kementan yang disusun / dikoreksi</td>
+                            <td class="tg-0pky">300</td>
+                            <td class="tg-0pky">&lt; 150</td>
+                            <td class="tg-0pky">&gt; 300</td>
+                            <td class="tg-0pky">Probis yang ditetapkan</td>
+                            <td class="tg-0pky">Jenis Penyelarasan : Direct Cascading / Non Direct Cascading (coret
+                                salah satu) <div class="square"></div>Catatan Perbaikan :<br><br></td>
+                        </tr>
+                        <tr>
+                            <td class="tg-0pky"></td>
+                            <td class="tg-0pky">IKI (Waktu)</td>
+                            <td class="tg-0pky">Ketepatan waktu penyelesaian proses bisnis sesuai dengan jadwal yang
+                                ditetapkan</td>
+                            <td class="tg-0pky">10</td>
+                            <td class="tg-0pky">&gt; 10</td>
+                            <td class="tg-0pky">&lt; 10</td>
+                            <td class="tg-0pky">Penilaian pejabat penilai</td>
+                            <td class="tg-0pky"></td>
+                        </tr>
+                        <tr>
+                            <td class="tg-0pky">Penataan Dokumen SOP Lingkup Kementan</td>
+                            <td class="tg-0pky">IKI (Kuantitas)</td>
+                            <td class="tg-0pky">Persentase SOP Makro dan Mikro yang selesai disusun/ dikoreksi</td>
+                            <td class="tg-0pky">90</td>
+                            <td class="tg-0pky">&lt; 50 </td>
+                            <td class="tg-0pky">&gt; 90</td>
+                            <td class="tg-0pky">SOP yang ditetapkan</td>
+                            <td class="tg-0pky">Jenis Penyelarasan : Direct Cascading / Non Direct Cascading (coret
+                                salah satu) <div class="square"></div>Catatan Perbaikan :<br><br></td>
+                        </tr>
+                        <tr>
+                            <td class="tg-0pky"></td>
+                            <td class="tg-sg5v">IKI (Waktu)</td>
+                            <td class="tg-0pky">Ketepatan waktu penyelesaian SOP sesuai dengan jadwal yang ditetapkan
+                            </td>
+                            <td class="tg-0pky">10</td>
+                            <td class="tg-0pky">&gt; 10</td>
+                            <td class="tg-0pky">&lt; 10</td>
+                            <td class="tg-0pky">Penilaian pejabat penilai</td>
+                            <td class="tg-0pky"></td>
+                        </tr>
+                        <tr>
+                            <td class="tg-c3ow" rowspan="2">2</td>
+                            <td class="tg-c3ow">Pelaksanaan Tugas Manajerial Kelompok Tata Laksana dan RB</td>
+                            <td class="tg-0pky">Pelaksanaan Tugas Manajerial Subkelompok Tata Laksana</td>
+                            <td class="tg-0pky">IKI (Kuantitas)</td>
+                            <td class="tg-0pky">Dokumen / Laporan Pelaksanaan Tugas Manajerial Subkelompok Tata Laksana
+                            </td>
+                            <td class="tg-0pky">12</td>
+                            <td class="tg-0pky">&lt; 12</td>
+                            <td class="tg-0pky">12</td>
+                            <td class="tg-0pky">Penilaian pejabat penilai</td>
+                            <td class="tg-0pky">Jenis Penyelarasan : Direct Cascading / Non Direct Cascading (coret
+                                salah satu) <div class="square"></div>Catatan Perbaikan :<br><br></td>
+                        </tr>
+                        <tr>
+                            <td class="tg-c3ow"></td>
+                            <td class="tg-0pky"></td>
+                            <td class="tg-0pky">IKI (Waktu)</td>
+                            <td class="tg-0pky">Ketepatan waktu penyelesaian tugas-tugas manajerial sesuai dengan jadwal
+                                yang ditetapkan</td>
+                            <td class="tg-0pky">12</td>
+                            <td class="tg-0pky">&gt; 10</td>
+                            <td class="tg-0pky">&lt; 10</td>
+                            <td class="tg-0pky">Penilaian pejabat penilai</td>
+                            <td class="tg-0pky"></td>
+                        </tr>
+                        <tr>
+                            <td class="tg-0pky" colspan="10">B. KINERJA TAMBAHAN</td>
+                        </tr>
+                        <tr>
+                            <td class="tg-c3ow">1</td>
+                            <td class="tg-0pky"></td>
+                            <td class="tg-0pky">Tim Pengelolaan Kinerja PNS</td>
+                            <td class="tg-0pky">IKI (Kuantitas)</td>
+                            <td class="tg-0pky">Laporan Hasil Pengelolaan Kinerja PNS Lingkup BPPSDMP</td>
+                            <td class="tg-0pky">12</td>
+                            <td class="tg-0pky">&lt; 12</td>
+                            <td class="tg-0pky">12</td>
+                            <td class="tg-0pky">Penilaian pejabat penilai</td>
+                            <td class="tg-0pky">Catatan Perbaikan :</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+
+                <table class="tc" style="table-layout: fixed; width: 194px">
+                    <colgroup>
+                        <col style="width: 194px">
+                    </colgroup>
+                    <thead>
+                        <tr>
+                            <th class="tc-jytt">(tempat), (tanggal, bulan, tahun)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="tc-jytt">Pengelola kinerja</td>
+                        </tr>
+                        <tr>
+                            <td class="tc-jytt" rowspan="4" height=30mm></td>
+                        </tr>
+                        <tr ></tr>
+                        <tr></tr>
+                        <tr></tr>
+                        <tr>
+                            <td class="tc-jytt">(Nama)</td>
+                        </tr>
+                        <tr>
+                            <td class="tc-jytt">(NIP)</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
